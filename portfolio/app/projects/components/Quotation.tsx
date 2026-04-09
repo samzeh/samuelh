bcf2103efc7b447b71a497bac5c5c30aa9007ac0
Samuel <124836035+samzeh@users.noreply.github.com>
2026-04-09 13:53:14 -0400

    fix quotation component vertical spacing

--- portfolio/app/projects/components/Quotation.tsx
+++ portfolio/app/projects/components/Quotation.tsx
@@ -1,10 +1,9 @@
 export default function Quotation({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
-      {/* Left accent line with rounded corners */}
+    <div className="flex my-4">
+
       <div className="w-1 bg-[#F5900B] rounded-full mr-3" />
       
-      {/* Quote text */}
       <blockquote className="italic text-detail-color tracking-normal">
         {children}
       </blockquote>
